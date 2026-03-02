e4f196a94267aa9ea271cbed07baa895b30c7abf
Jerry Richards <Jerry.Richards@Outlook.com>
2026-03-02 16:19:28 -0700

    Expanded Lottie animations to three. Scene Animations to two. Forward .NET MAUI events to JavaScript in Lottie HTML.

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -26,13 +26,21 @@ public partial class MainPage : ContentPage
     {
     };
 
+    WebView _selectedLottie;
+    WebView _selectedAnimation;
+
     public MainPage(MainPageModel model)
     {
         InitializeComponent();
         BindingContext = model;
 
         Loaded += async (s, e) => {
-            MainWebView.Source = await GetModelAsync(BabylonModels.LottieModel);
+            LottieViewLeft.Source = await GetModelAsync(BabylonModels.LottieModel);
+            LottieViewMiddle.Source = await GetModelAsync(BabylonModels.LottieModel);
+            LottieViewRight.Source = await GetModelAsync(BabylonModels.LottieModel);
+
+            AnimationViewLeft.Source = await GetModelAsync(BabylonModels.PlaneModel);
+            AnimationViewRight.Source = await GetModelAsync(BabylonModels.PlaneModel);
         };
     }
 
@@ -51,5 +59,142 @@ public partial class MainPage : ContentPage
             default:
                 return babylonPlaneModel;
         }
+    }
+
+    void OnLottieFocused(object sender, FocusEventArgs e)
+    {
+        _selectedLottie = (WebView)sender;
+        UpdateLottieFrameBorders();
+    }
+
+    void OnLottieUnfocused(object sender, FocusEventArgs e)
+    {
+        UpdateLottieFrameBorders();
+    }
+
+    void UpdateLottieFrameBorders()
+    {
+        LottieLeftFrame.Stroke = Colors.Transparent;
+        LottieMiddleFrame.Stroke = Colors.Transparent;
+        LottieRightFrame.Stroke = Colors.Transparent;
+
+        if (_selectedLottie == LottieViewLeft)
+            LottieLeftFrame.Stroke = Colors.Cyan;
+        else if (_selectedLottie == LottieViewMiddle)
+            LottieMiddleFrame.Stroke = Colors.Cyan;
+        else if (_selectedLottie == LottieViewRight)
+            LottieRightFrame.Stroke = Colors.Cyan;
+    }
+
+    private void OnPlayLottieClicked(object sender, EventArgs e)
+    {
+        PlaySelectedLottie();
+    }
+
+    private void OnPauseLottieClicked(object sender, EventArgs e)
+    {
+        PauseSelectedLottie();
+    }
+
+    private void OnStopLottieClicked(object sender, EventArgs e)
+    {
+        StopSelectedLottie();
+    }
+
+    public void PlaySelectedLottie()
+    {
+        if (_selectedLottie == null)
+        {
+            DisplayAlert("No Selection", "Please select a Lottie first by clicking on it", "OK");
+            return;
+        }
+        _selectedLottie?.EvaluateJavaScriptAsync("window.play()");
+    }
+
+    public void PauseSelectedLottie()
+    {
+        if (_selectedLottie == null)
+        {
+            DisplayAlert("No Selection", "Please select a Lottie first by clicking on it", "OK");
+            return;
+        }
+        _selectedLottie?.EvaluateJavaScriptAsync("window.pause()");
+    }
+
+    public void StopSelectedLottie()
+    {
+        if (_selectedLottie == null)
+        {
+            DisplayAlert("No Selection", "Please select a Lottie first by clicking on it", "OK");
+            return;
+        }
+        _selectedLottie?.EvaluateJavaScriptAsync("window.stop()");
+    }
+
+    void OnAnimationFocused(object sender, FocusEventArgs e)
+    {
+        _selectedAnimation = (WebView)sender;
+        UpdateAnimationFrameBorders();
+    }
+
+    void OnAnimationUnfocused(object sender, FocusEventArgs e)
+    {
+        UpdateAnimationFrameBorders();
+    }
+
+    void UpdateAnimationFrameBorders()
+    {
+        AnimationLeftFrame.Stroke = Colors.Transparent;
+        AnimationRightFrame.Stroke = Colors.Transparent;
+
+        if (_selectedAnimation == AnimationViewLeft)
+            AnimationLeftFrame.Stroke = Colors.Cyan;
+        else if (_selectedAnimation == AnimationViewRight)
+            AnimationRightFrame.Stroke = Colors.Cyan;
+    }
+
+    private void OnPlayAnimationClicked(object sender, EventArgs e)
+    {
+        PlaySelectedAnimation();
+    }
+
+    private void OnPauseAnimationClicked(object sender, EventArgs e)
+    {
+        PauseSelectedAnimation();
+    }
+
+    private void OnStopAnimationClicked(object sender, EventArgs e)
+    {
+        StopSelectedAnimation();
+    }
+
+    public void PlaySelectedAnimation()
+    {
+        if (_selectedAnimation == null)
+        {
+            DisplayAlert("No Selection", "Please select an animation first by clicking on it", "OK");
+            return;
+        }
+        _selectedAnimation?.EvaluateJavaScriptAsync("window.play()");
+    }
+
+    public void PauseSelectedAnimation()
+    {
+        if (_selectedAnimation == null)
+        {
+            DisplayAlert("No Selection", "Please select an animation first by clicking on it", "OK");
+            return;
+        }
+        _selectedAnimation?.EvaluateJavaScriptAsync("window.pause()");
+    }
+
+    public void StopSelectedAnimation()
+    {
+        if (_selectedAnimation == null)
+        {
+            DisplayAlert("No Selection", "Please select an animation first by clicking on it", "OK");
+            return;
+        }
+        _selectedAnimation?.EvaluateJavaScriptAsync("window.stop()");
     }
 }
